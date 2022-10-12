aa79c5416eef4191e5790a5b8887ab11b2dc9978
Bryan Tavares <bryantavares25@gmail.com>
2022-10-12 15:20:20 -0300

    News

---
+++ Basic_sintax_01.py
@@ -0,0 +1,37 @@
+#Atividade 01 - Sintaxe Básica
+
+#Exercício 01
+linguagem = 'python'
+
+print (linguagem)
+
+#Exercício 02
+a = 1500
+b = 335
+
+print(a+b)
+print(a*b)
+print(a-b)
+
+#Exercício 03
+a = 'Marie'
+b = ' Curie'
+c = '!!!'
+
+print(a+b+c)
+
+#Exercício 04
+print(2+3==5)
+
+#Exercício 05
+print(5+9==13)
+
+#Exercício 06
+pi = 3.1415
+
+print(pi)
+
+#Exercise 07
+pi = 3.1415
+    
+print(pi)

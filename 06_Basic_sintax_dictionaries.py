c501bd1ba4e7c6ceaabf9c58296ba0059331f426
Bryan Tavares <bryantavares25@gmail.com>
2022-10-29 14:17:58 -0300

    Update 06_Basic_sintax_dictionaries.py

--- 06_Basic_sintax_dictionaries.py
+++ 06_Basic_sintax_dictionaries.py
@@ -5,6 +5,15 @@ print('\n', '-'*15, 'Exercício 1', '-'*15, '\n')
 
 pdb = {'1A8M': 471, '1TNR': 283, '2AZ5': 592, '1TNF': 471, '2TNF': 468, '2TUN': 942, '4TSV': 150, '5TSW': 900, '2E7A': 471, '6RMJ': 489}
 
-print(f'{pdb('2TNF')}')
+print(f'\nTamanho de 2TNF: {pdb["2TNF"]}')
+print(f'\nTamanho de 2E7A: {pdb["2E7A"]}')
+
+print(f'\nO dicionário tem o tamanho de:  {len(pdb)}')
+
+print(f'\nAs chaves do dicionário: {pdb.keys()}')
+
+print(f'\nOs valores do dicionário: {pdb.values()}')
+
+print(f'\nOs itens do dicionário: {pdb.items()}')
 
 # Rm 11:36 <º)))>< B. A. R. T.

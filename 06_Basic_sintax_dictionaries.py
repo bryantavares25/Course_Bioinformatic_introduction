b2a8c91e701f6799aa67c19ed581aa9eea462486
Bryan Tavares <bryantavares25@gmail.com>
2022-10-29 11:49:43 -0300

    Update 06_Basic_sintax_dictionaries.py

--- 06_Basic_sintax_dictionaries.py
+++ 06_Basic_sintax_dictionaries.py
@@ -2,3 +2,9 @@
 
 #Exercício 1
 print('\n', '-'*15, 'Exercício 1', '-'*15, '\n')
+
+pdb = {'1A8M': 471, '1TNR': 283, '2AZ5': 592, '1TNF': 471, '2TNF': 468, '2TUN': 942, '4TSV': 150, '5TSW': 900, '2E7A': 471, '6RMJ': 489}
+
+print(f'{pdb}')
+
+# Rm 11:36 <º)))>< B. A. R. T.

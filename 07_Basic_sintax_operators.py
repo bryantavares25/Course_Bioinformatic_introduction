0bae7c7879301b9877c22468e1dba4a5b61a4dfb
Bryan Tavares <bryantavares25@gmail.com>
2022-11-02 15:53:04 -0300

    Update 07_Basic_sintax_operators.py

--- 07_Basic_sintax_operators.py
+++ 07_Basic_sintax_operators.py
@@ -19,7 +19,24 @@ a5 = 5
 
 am = (a1+a2+a3+a4+a5)/5
 
-print(f'A média dos números {a1}, {a2}, {a3}, {a4} e {a5}: {am}')
+print(f'A média entre os números {a1}, {a2}, {a3}, {a4} e {a5}: {am}')
 
+#Exercício 3
+print('\n', '-'*15, 'Exercício 3', '-'*15, '\n')
+
+b = 5
+e = 2
+
+print(f'Valor da base {b} elevada pelo expoente {e} é {b**e}')
+
+#Exercício 4
+print('\n', '-'*15, 'Exercício 4', '-'*15, '\n')
+
+n1 = 1001
+n2 = 2
+
+nt = 0 == n1%n2
+
+print(f'O número {n1} é divisível por {n2}: {nt}')
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

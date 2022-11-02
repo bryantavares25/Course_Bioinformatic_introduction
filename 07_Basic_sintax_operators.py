5620ba36ca2d63e2c93382efd2c92de19e310e38
Bryan Tavares <bryantavares25@gmail.com>
2022-11-02 15:40:20 -0300

    Update 07_Basic_sintax_operators.py

--- 07_Basic_sintax_operators.py
+++ 07_Basic_sintax_operators.py
@@ -3,6 +3,23 @@
 #Exercício 1
 print('\n', '-'*15, 'Exercício 1', '-'*15, '\n')
 
+a = 10
+b = 11
+
+print(f'O número {a} é igual ao número {b}: {a==b}')
+
+#Exercício 2
+print('\n', '-'*15, 'Exercício 2', '-'*15, '\n')
+
+a1 = 1
+a2 = 2
+a3 = 3
+a4 = 4
+a5 = 5
+
+am = (a1+a2+a3+a4+a5)/5
+
+print(f'A média dos números {a1}, {a2}, {a3}, {a4} e {a5}: {am}')
 
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

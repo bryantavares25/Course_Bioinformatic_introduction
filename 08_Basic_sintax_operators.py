2b48c29d85c9bd9d15cfcf35d81126def4315214
Bryan Tavares <bryantavares25@gmail.com>
2022-11-02 21:15:27 -0300

    Update 08_Basic_sintax_operators.py

--- 08_Basic_sintax_operators.py
+++ 08_Basic_sintax_operators.py
@@ -1,33 +1,41 @@
 #Atividade 7 - Sintaxe básica: Operadores
 
+import math
+
 #Exercício 1
 print('\n', '-'*15, 'Exercício 1', '-'*15, '\n')
 
-# Glicina 1 : ATOMO x y z
-N_01 = (108.304, 100.827, 67.992)
-CA_01 = (108.477, 100.389, 69.362)
-C_01 = (109.907, 100.555, 69.817)
-O_01 = (110.821, 100.799, 69.027)
+# Resíduos de glicina
 
+g1 = [[108.304, 100.827, 67.992], [108.477, 100.389, 69.362], [109.907, 100.555, 69.817], [110.821, 100.799, 69.027]]
+g2 = [[107.670, 101.359, 70.074], [108.477, 100.389, 69.362], [109.513, 101.011, 68.450], [110.667, 100.572, 68.425]]
 
-# Glicina 2 : ATOMO x y z
-N_02 = (107.670, 101.359, 70.074)
-CA_02 = (108.477, 100.389, 69.362)
-C_02 = (109.513, 101.011, 68.450)
-O_02 = (110.667, 100.572, 68.425)
+print(f'Glicina 1: {g1}\n')
+print(f'Glicina 2: {g2}\n')
 
 #RMSD (Root Mean Square Deviation)
 
-soma1 = (N_01[0] - N_02[0])**2 + (N_01[1] - N_02[1])**2 + (N_01[2] - N_02[2])**2
-soma2 = (CA_01[0] - CA_02[0])**2 + (CA_01[1] - CA_02[1])**2 + (CA_01[2] - CA_02[2])**2
-soma3 = (C_01[0] - C_02[0])**2 + (C_01[1] - C_02[1])**2 + (C_01[2] - C_02[2])**2
-soma4 = (O_01[0] - O_02[0])**2 + (O_01[1] - O_02[1])**2 + (O_01[2] - O_02[2])**2
+# 1º Soma para cada um dos átomos
+temN = (g1[0][0] - g2[0][0])**2 + (g1[0][1] - g2[0][1])**2 + (g1[0][2] - g2[0][2])**2
+temCA = (g1[1][0] - g2[1][0])**2 + (g1[1][1] - g2[1][1])**2 + (g1[1][2] - g2[1][2])**2
+temCB = (g1[2][0] - g2[2][0])**2 + (g1[2][1] - g2[2][1])**2 + (g1[2][2] - g2[2][2])**2
+temO = (g1[3][0] - g2[3][0])**2 + (g1[3][1] - g2[3][1])**2 + (g1[3][2] - g2[3][2])**2
 
-print(soma1)
-print(soma2)
-print(soma3)
-print(soma4)
+# 2º Soma de todos os átomos
+soma = temN + temCA + temCB + temO
 
-print(soma1 + soma2 + soma3 + soma4)
+# 3º Divisão da soma pela quantidade de átomos pareados
+quoc = soma/len(g1)
+
+# 4º Raíz quadrada da divisão
+raiz = math.sqrt(quoc)
+
+# Valor final de RMSD
+print(f'O RMSD entre os dois resíduos de glicina é: {raiz}')
+
+#Exercício 2
+print('\n', '-'*15, 'Exercício 2', '-'*15, '\n')
+
+
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

590aa1f6fe716f622ae6fc6a7ac02becb97e648f
Bryan Tavares <bryantavares25@gmail.com>
2022-12-09 09:14:02 -0300

    Update 11_Basic_sintax_repeating_structures.py

--- 11_Basic_sintax_repeating_structures.py
+++ 11_Basic_sintax_repeating_structures.py
@@ -3,6 +3,16 @@
 #Exercício 1
 print('\n', '-'*15, 'Exercício 1', '-'*15, '\n')
 
+for i in range(0,11):
+    print(i, end=' ')
+
+num = 0
+
+while num < 11:
+    print(num, end=' ')
+    num = num + 1
+
+print(' Teste ')
 
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

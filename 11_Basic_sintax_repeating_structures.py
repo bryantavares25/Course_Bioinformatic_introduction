c340f94685a70836c41ed1073c26cf5fa5cf47ce
Bryan Tavares <bryantavares25@gmail.com>
2022-12-09 09:36:01 -0300

    Update 11_Basic_sintax_repeating_structures.py

--- 11_Basic_sintax_repeating_structures.py
+++ 11_Basic_sintax_repeating_structures.py
@@ -49,6 +49,17 @@ for i in sedna:
 
 print('')
 
+#Exercício 4
 print('\n', '-'*15, 'Exercício 4', '-'*15, '\n')
 
+#Escreva um programa em Python que calcule e imprima todos os divisores dos números entre 1 e 100.
+
+for q in range(1,101):
+    for w in range(1,q):
+        print(f'Divisores de {q}:', end=' ')
+        if q%w==0:
+            print(f'{w}', end=' ')
+        else:
+            print('')
+    print('TEste 4')
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

d5e6725d57d65932a9548e08c314b42ef4e52d7e
Bryan Tavares <bryantavares25@gmail.com>
2022-12-10 15:59:46 -0300

    Update 12_Basic_sintax_repeating_structures.py

--- 12_Basic_sintax_repeating_structures.py
+++ 12_Basic_sintax_repeating_structures.py
@@ -17,9 +17,15 @@ PROTEINA = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q'
 
 seqlist = ['ATCDLASKWNWNHTLCAAHCIARRYRGGYCNSKAVCVCRN', 'TATTAACCGGGTTTAAACTAGCATGCATGATTAACCAGTACATCTTTT', 'ATCBDLASKWXWNHTLCAAHCIARRYRGGYCNSJAVCVCRN']
 
+# Estrutura de repetição para cada item da lista
 for i in range(0,len(seqlist)):
     print(seqlist[i])
+
+    # Estrutura de repetição para cada item do texto
     for o in seqlist[i]:
+        #Teste DNA
+        ehDNA = True
+        erro = []
         print(o)
             
 print('FIM')

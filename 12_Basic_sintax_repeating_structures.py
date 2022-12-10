f18cccbb04d6f76366918f65db019736d2016918
Bryan Tavares <bryantavares25@gmail.com>
2022-12-10 20:10:19 -0300

    Update 12_Basic_sintax_repeating_structures.py

--- 12_Basic_sintax_repeating_structures.py
+++ 12_Basic_sintax_repeating_structures.py
@@ -99,6 +99,22 @@ print(f'\nO valor do fatorial de {numfat} é {fat}.')
 #Exercício 5
 print('\n', '-'*15, 'Exercício 5', '-'*15, '\n')
 
+print('TABUADAS')
+
+for z in range(1,16):   
+    print(f'- - Tabuada do número {z} - -')
+    for x in range(1,11):
+        print(f'> {z} X {x} = {z*x}')
+
+#Exercício 6
+print('\n', '-'*15, 'Exercício 6', '-'*15, '\n')
+
+prot = 'VRSSSRTPSDKPVAHVVANPQAEGQLQWLNRRANALLANGVELRDNQLVVPSEGLYLIYSQVLFKGQGCPSTHVLLTHTISRIAVSYQTKVNLLSAIKSPCQRETPEGAEAKPWYEPIYLGGVFQLEKGDRLSAEINRPDYLLFAESGQVYFGIIAL'
+
+aamass = {'A':71.03711, 'C':103.00919, 'D':115.02694, 'E':129.04259, 'F':147.06841, 'G':57.02146, 'H':137.05891, 'I':113.08406, 'K':128.09496, 'L':113.08406, 'M':131.04049, 'N':114.04293, 'P':97.05276, 'Q':128.05858, 'R':156.10111, 'S':87.03203, 'T':101.04768, 'V':99.06841, 'W':186.07931, 'Y':163.06333}
+
+massamolar = 0
+
 
 
 

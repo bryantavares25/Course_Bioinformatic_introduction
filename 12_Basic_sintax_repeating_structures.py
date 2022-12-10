73d2efb77c79f9d938e06800ab0c2565600fb0d5
Bryan Tavares <bryantavares25@gmail.com>
2022-12-10 14:46:07 -0300

    Update 12_Basic_sintax_repeating_structures.py

--- 12_Basic_sintax_repeating_structures.py
+++ 12_Basic_sintax_repeating_structures.py
@@ -9,12 +9,15 @@ for fah in range (1,151):
 #Exercício 2
 print('\n', '-'*15, 'Exercício 2', '-'*15, '\n')
 
-#2. Utilizando as estruturas de repetição, verifique se as seguintes sequências são uma sequência de DNA {A,
-#G, T, C}, RNA {U, C, A, G}, PROTEÍNA {A, C, D, E, F, G, H, I, K, L, M, N, P, Q, R, S, T, V, W, Y} ou nenhuma
-#delas (nesse caso, imprima as letras que não fazem parte do alfabeto):
-#ATCDLASKWNWNHTLCAAHCIARRYRGGYCNSKAVCVCRN
-#TATTAACCGGGTTTAAACTAGCATGCATGATTAACCAGTACATCTTTT
-#ATCBDLASKWXWNHTLCAAHCIARRYRGGYCNSJAVCVCRN
+#2. Utilizando as estruturas de repetição, verifique se as seguintes sequências são uma sequência de 
+DNA = {A,G, T, C}
+RNA = {U, C, A, G}
+PROTEÍNA = {A, C, D, E, F, G, H, I, K, L, M, N, P, Q, R, S, T, V, W, Y} 
+# ou nenhuma delas (nesse caso, imprima as letras que não fazem parte do alfabeto):
+
+seq1 = 'ATCDLASKWNWNHTLCAAHCIARRYRGGYCNSKAVCVCRN'
+seq2 = 'TATTAACCGGGTTTAAACTAGCATGCATGATTAACCAGTACATCTTTT'
+seq3 = 'ATCBDLASKWXWNHTLCAAHCIARRYRGGYCNSJAVCVCRN'
 
 
 

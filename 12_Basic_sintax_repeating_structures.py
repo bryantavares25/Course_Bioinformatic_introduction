336732c2a60560e7e4577c8ebb69ae9303b59ea6
Bryan Tavares <bryantavares25@gmail.com>
2022-12-10 15:44:47 -0300

    Update 12_Basic_sintax_repeating_structures.py

--- 12_Basic_sintax_repeating_structures.py
+++ 12_Basic_sintax_repeating_structures.py
@@ -9,16 +9,20 @@ for fah in range (1,151):
 #Exercício 2
 print('\n', '-'*15, 'Exercício 2', '-'*15, '\n')
 
-#2. Utilizando as estruturas de repetição, verifique se as seguintes sequências são uma sequência de 
-DNA = {A,G, T, C}
-RNA = {U, C, A, G}
-PROTEÍNA = {A, C, D, E, F, G, H, I, K, L, M, N, P, Q, R, S, T, V, W, Y} 
+#Verifique se as seguintes sequências são uma sequência de 
+DNA = {'A', 'G', 'T', 'C'}
+RNA = {'U', 'C', 'A', 'G'}
+PROTEINA = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'} 
 # ou nenhuma delas (nesse caso, imprima as letras que não fazem parte do alfabeto):
 
-seq1 = 'ATCDLASKWNWNHTLCAAHCIARRYRGGYCNSKAVCVCRN'
-seq2 = 'TATTAACCGGGTTTAAACTAGCATGCATGATTAACCAGTACATCTTTT'
-seq3 = 'ATCBDLASKWXWNHTLCAAHCIARRYRGGYCNSJAVCVCRN'
+seqlist = ['ATCDLASKWNWNHTLCAAHCIARRYRGGYCNSKAVCVCRN', 'TATTAACCGGGTTTAAACTAGCATGCATGATTAACCAGTACATCTTTT', 'ATCBDLASKWXWNHTLCAAHCIARRYRGGYCNSJAVCVCRN']
 
+for i in range(0,len(seqlist)):
+    print(seqlist[i])
+    for o in seqlist[i]:
+        print(o)
+            
+print('FIM')
 
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

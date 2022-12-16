9c761a9b5ca2a616f14f2e938a3eaee21e555334
Bryan Tavares <bryantavares25@gmail.com>
2022-12-16 10:46:57 -0300

    Create 13_challenge_01.py

---
+++ Module_3/13_challenge_01.py
@@ -0,0 +1,15 @@
+#Atividade 12 - Sintaxe básica: Estruturas de repetição
+
+# Projete um algoritmo e implemente uma função em Python que receba como entrada uma lista e retorne o maior elemento desse conjunto.
+# Descubra quais as operações mais relevantes em termos de tempo de execução de seu programa.
+# Tente calcular a função de complexidade de tempo f(n) de seu programa.
+# Você acha que seu programa é ótimo?
+
+
+#Exercício 1
+print('\n', '-'*15, 'Exercício 1', '-'*15, '\n')
+
+inplist = []
+
+
+# B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

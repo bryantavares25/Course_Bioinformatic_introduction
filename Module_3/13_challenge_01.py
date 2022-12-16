6790e5b898195524a8e43d67f722764a6b3cc246
Bryan Tavares <bryantavares25@gmail.com>
2022-12-16 11:31:05 -0300

    Update 13_challenge_01.py

--- Module_3/13_challenge_01.py
+++ Module_3/13_challenge_01.py
@@ -1,6 +1,5 @@
 #Atividade 12 - Sintaxe básica: Estruturas de repetição
 
-# Projete um algoritmo e implemente uma função em Python que receba como entrada uma lista e retorne o maior elemento desse conjunto.
 # Descubra quais as operações mais relevantes em termos de tempo de execução de seu programa.
 # Tente calcular a função de complexidade de tempo f(n) de seu programa.
 # Você acha que seu programa é ótimo?
@@ -16,24 +15,11 @@ def maior(a):
         if i > k:
             k = i
     
-    print(f'{k} é o maior valor da lista')
+    print(f'O elemento de maior valor da lista: {k}.')
 
 inplist = [7, 1, 2, 3, 5]
 
-print(f'A partir da lista: {inplist}.\nResultado: ', end='')
+print(f'A partir da lista: {inplist}.', end='')
 maior(inplist)
-
-# Estrutura funcional de uma classe
-#class Proteína:
-#    def __init__(self, nome, tamanho):
-#        self.nome = nome
-#        self.tamanho = tamanho
-#    
-#    def minhafuncao(self):
-#        print('Proteína: ' + self.nome)
-#
-#p1 = Proteína('Horácio', 237)
-#
-#p1.minhafuncao()
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

0c030670c84d516d21c54b086cfd37d7101b2e09
Bryan Tavares <bryantavares25@gmail.com>
2022-12-16 16:34:55 -0300

    Update 15_challenge_03.py

--- Module_3/15_challenge_03.py
+++ Module_3/15_challenge_03.py
@@ -3,14 +3,28 @@
 #Dasafio 1
 print('\n', '-'*15, 'Desafio 1', '-'*15, '\n')
 
-seqcres = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
 def pesbi(reg, lis):
     
-    meio = len(lis)/2
-    for i in lis[meio]:
-        if lis[i] > reg:
-            print('')
+    if len(lis) == 0:
+        return -1
+    
+    esq = 0
+    dir = len(lis)-1
+
+    meio = len(lis)//2
+
+    i = int((esq+dir)/2)
+    
+    while esq<= dir and reg != lis[i]:
+        if lis[meio] > reg:
+            meio = (lis[meio]//2)+lis[meio]
         else:
-            print('')
+            meio = lis[meio]//2
+    return (reg, meio)
+
+seqcres = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+j = pesbi(2,seqcres)
+
+print(j)
+
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

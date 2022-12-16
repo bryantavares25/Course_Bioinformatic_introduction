5f91a803ba58ef991ec615bc52f3a1274c379cce
Bryan Tavares <bryantavares25@gmail.com>
2022-12-16 15:15:15 -0300

    Create 14_challenge_02.py

---
+++ Module_3/14_challenge_02.py
@@ -0,0 +1,13 @@
+#Atividade 14 - Desafios: Análise de algoritmos
+
+#Dasafio 1
+print('\n', '-'*15, 'Desafio 1', '-'*15, '\n')
+
+trm = ['ABC', 'ABD', 'ABE']
+
+for i in trm:
+    if i == 'ABE':
+        print(i)
+
+
+# B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

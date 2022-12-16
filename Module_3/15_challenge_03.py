7ca6b142a730c09cb90d8c43a338ea009e693964
Bryan Tavares <bryantavares25@gmail.com>
2022-12-16 16:22:32 -0300

    Create 15_challenge_03.py

---
+++ Module_3/15_challenge_03.py
@@ -0,0 +1,16 @@
+#Atividade 14 - Desafios: Análise de algoritmos
+
+#Dasafio 1
+print('\n', '-'*15, 'Desafio 1', '-'*15, '\n')
+
+seqcres = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def pesbi(reg, lis):
+    
+    meio = len(lis)/2
+    for i in lis[meio]:
+        if lis[i] > reg:
+            print('')
+        else:
+            print('')
+# B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

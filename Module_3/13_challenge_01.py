6e5ecdae22ebcd15e1c579da5703b3889af63f7a
Bryan Tavares <bryantavares25@gmail.com>
2022-12-16 11:17:18 -0300

    Update 13_challenge_01.py

--- Module_3/13_challenge_01.py
+++ Module_3/13_challenge_01.py
@@ -9,14 +9,31 @@
 #Exercício 1
 print('\n', '-'*15, 'Exercício 1', '-'*15, '\n')
 
-inplist = []
-
-def maior(list):
+def maior(a):
     k = 0
-    for i in inplist:
+
+    for i in a:
         if i > k:
             k = i
+    
+    print(f'{k} é o maior valor da lista')
 
+inplist = [7, 1, 2, 3, 5]
 
+print(f'A partir da lista: {inplist}.\nResultado: ', end='')
+maior(inplist)
+
+# Estrutura funcional de uma classe
+#class Proteína:
+#    def __init__(self, nome, tamanho):
+#        self.nome = nome
+#        self.tamanho = tamanho
+#    
+#    def minhafuncao(self):
+#        print('Proteína: ' + self.nome)
+#
+#p1 = Proteína('Horácio', 237)
+#
+#p1.minhafuncao()
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

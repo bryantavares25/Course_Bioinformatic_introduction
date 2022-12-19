dac4a60af4695a66655f6e4581d999df80a7abcd
Bryan Tavares <bryantavares25@gmail.com>
2022-12-19 20:05:31 -0300

    Update 16_challenge_04.py

--- Module_4/16_challenge_04.py
+++ Module_4/16_challenge_04.py
@@ -3,6 +3,26 @@
 #Dasafio 4
 print('\n', '-'*15, 'Desafio 4', '-'*15, '\n')
 
+#Distância de Hamming
+def dham(seq1, seq2):
+
+    dis = sem = 0
+
+    #Contar as modificações necessárias para que as sequências se tornem identicas
+    for let in range(0, len(seq1)):
+        if seq1[let] == seq2[let]:
+            sem = sem + 1
+        else:
+            dis = dis+1
+
+    return dis
+
+#def dlev(): #2. A distância de Levenshtein
+
+ss = 'AAC'
+aa = 'AAB'
+
+print(f'Distância de Hamming: {dham}')
 
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <

8927e7cf57f96b5ac19fa7a63290dac7f8bd177e
Bryan Tavares <bryantavares25@gmail.com>
2022-12-20 13:51:34 -0300

    Update 17_challenge_05.py

--- Module_4/17_challenge_05.py
+++ Module_4/17_challenge_05.py
@@ -11,17 +11,36 @@ def lcs(v, w):
     pontuacao = [0]*len(v)
     ponteiros = ['']*len(v)
 
+    # Inicialiazação de duas matrizes vazias
     for i in range(0,len(w)):
         pontuacao[i] = [0]*len(v)
         ponteiros[i] = ['']*len(v)
 
+    # i para linhas, j para colunas
     for i in range(0,len(w)):
         ponteiros[i][0] = '|'
-    
-    for i in range(0, len(v)):
+    for j in range(0, len(v)):
         ponteiros[0][i] = '_'
+
+    for i in range(1,len(w)):
+        for j in range(1, len(v)):
+            pontuacao[i][j] = max(v[j], w[i], pontuacao[i-1][j], pontuacao[i][j-1], pontuacao[i-1][j-1])
+            ponteiros[i][j] = pon(v[j], w[i], pontuacao[i-1][j], pontuacao[i][j-1], pontuacao[i-1][j-1])
+
+    impmatriz =
+    alinhafin =
+
+def max(s1, s2, c, l, d):
+    if (s1 == s2 and (d+1) >= c and d+1 >= l):
+        d = d+1
+        return d
+    elif (l >= c and l >= d):
+        return l
+    else:
+        return c
 
 seq1 = ['*','A', 'A', 'C', 'G', 'T', 'T', 'A', 'T', 'G']
 seq2 = ['*', 'A', 'C', 'C', 'G', 'T', 'T', 'G', 'T', 'G']
+
 
 # B. A. R. T. > < ( ( (º > Rm 11:36 < º ) ) ) > <
